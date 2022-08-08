f026c9113bd50fb696520732aabc2398dc5c9e6a
funtionup-aug <arungadhe11@gmail.com>
2022-08-08 22:39:29 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,48 +1,112 @@
+const { request } = require('express');
 const express = require('express');
-const lodash = require('lodash')
 const abc = require('../introduction/intro')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    res.send('My second ever api!')
+// router.get('/test-me', function (req, res) {
+//     console.log('My batch is', abc.name)
+//     abc.printName()
+//     logger.welcome()
 
-    let month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    console.log(lodash.chunk(month, 4))
+//     res.send('My second ever api!')
+// });
 
-    let oddNum = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19.]
+router.get('/students', function (req, res) {
+    let students = ['Sabiha', 'Neha', 'Akash']
+    res.send(students)
+})
+
+// router.get('/student-details/:name', function(req, res){
+//     /*
+//     params is an attribute inside request that contains 
+//     dynamic values.
+//     This value comes from the request url in the form of an 
+//     object where key is the variable defined in code 
+//     and value is what is sent in the request
+//     */
+
+//     let requestParams = req.params
+
+//     // JSON strigify function helps to print an entire object
+//     // We can use any ways to print an object in Javascript, JSON stringify is one of them
+//     console.log("This is the request "+ JSON.stringify(requestParams))
+//     let studentName = requestParams.name
+//     console.log('Name of the student is ', studentName)
+
+//     res.send('Dummy response')
+// })
+
+router.get('/GET/movies', function (req, res) {
+
+    let movie = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    res.send(movie)
+})
+
+
+router.get('/GET/movies/:indexNumber', function (req, res) {
+    let movieArray = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    let index = (req.params.indexNumber - 1)
+    if (index < movieArray.length) {
+        let movie = movieArray[index]
+        res.send(movie)
+
+    } else if (index > movieArray.length) {
+        res.send("Please Enter Valid Number")
+    }
+
+})
+
+
+router.get('/GET/films', function (req, res) {
+
+    const data = [{
+        id: 1,
+        name: "The Shining",
+    }, {
+        id: 2,
+        name: "Incendies"
+    }, {
+        id: 3,
+        name: "Rang de Basanti"
+    }, {
+        id: 4,
+        name: "Finding Nemo"
+    }]
+
+    res.send(data)
+
+})
+
+
+router.get('/GET/films/:filmId', function (req, res) {
+
+    const data = [{
+        id: 1,
+        name: "The Shining",
+    }, {
+        id: 2,
+        name: "Incendies"
+    }, {
+        id: 3,
+        name: "Rang de Basanti"
+    }, {
+        id: 4,
+        name: "Finding Nemo"
+    }]
+
+
+    let index = req.params.filmId - 1
+    if (index < data.length) {
+       var movie = data[index]
     
-    console.log(_.tail(oddNum))
+
+    } else {
+        res.send("Please Enter Valid Number")
+    }
+    res.send(movie)
 
 
-    let gfg = _.union([10000, 22, 3],
-        [3, 840, 10000],
-        [840, 22, 7],
-        [8, 99999, 10000],
-        [11, 12, 13]);
-
-    console.log(gfg)
-
-    let obj1 =[
-        ["horror","The Shining"],
-        ["drama","Titanic"],
-        ["thriller","Shutter Island"],
-        ["fantasy","Pans Labyrinth"]
-       ]
-
-    let obj2 =_.fromPairs(obj1);
-    console.log(obj2)
-
-});
-
-
-router.get('/test-you', function (req, res) {
-    res.send('This is the second routes implementation')
 })
 
-router.get('/give-me-students-data', function (req, res) {
 
-})
 module.exports = router;
-// adding this comment for no reason
