3d5c5c32cd5fc5aea29134332c2bfa360f084ab9
funtionup-aug <arungadhe11@gmail.com>
2022-08-25 11:54:22 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
-
+const moment = require("moment")
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,6 +13,24 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
+
+
+
+app.use(
+    function(req, res , next){
+
+    let date = moment().format("DD-MM-YYYY")
+
+   let ip = req.ip
+
+   let url =req.originalUrl
+   console.log(date,ip,url)
+
+   return res.send("this is our ans")
+
+    }
+)
+
 
 app.use (
     function (req, res, next) {
