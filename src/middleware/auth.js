7b79947e7f5593fedff7be69d29bd1daaf83ba4a
funtionup-aug <arungadhe11@gmail.com>
2022-09-01 18:10:16 +0530

    try and catch

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,15 +5,15 @@ const authentication = async function (req, res, next) {
     try {
         let token = req.headers["x-auth-token"];
         if (!token)
-            if (!token) return res.send({ status: false, msg: "token must be present" });
+            if (!token) return res.status(403).send({ status: false, msg: "token must be present" });
         console.log(token);
         let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
         if (!decodedToken)
-            return res.send()({ status: false, msg: "token is invalid" });
+            return res.status(403).send()({ status: false, msg: "token is invalid" });
         next()
     }
     catch (error) {
-        return res.send(500)({ status: false, msg: "Server Error" });
+        return res.status(500).send({ status: false, msg: "Server Error" });
     }
 };
 
@@ -25,12 +25,12 @@ try{
     let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key")
     let userToBeModified = req.params.userId
     let userLoggedIn = decodedToken.userId
-    if (!decodedToken) return res.send({ status: false, msg: "token in Valid" })
-    if (userToBeModified != userLoggedIn) return res.send({ status: false, msg: "You Are LoggedIn But Not Authorised For This Activity" })
+    if (!decodedToken) return res.status(403).send({ status: false, msg: "token in Valid" })
+    if (userToBeModified != userLoggedIn) return res.status(403).send({ status: false, msg: "You Are LoggedIn But Not Authorised For This Activity" })
     next()
 }
 catch {
-    res.send(500)({status:false,msg:"Server Error"})
+    res.status(500).send({status:false,msg:"Server Error"})
 }
 
 }
